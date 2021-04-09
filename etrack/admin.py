bf3c63fe17b2b4f5cfe6098370a71759b03583f1
ZOUMUNAN <zmn@zmnMBP2.local>
2021-04-10 00:18:09 +0900

    20210410_Z  Changes to be committed: 	modified:   .vscode/settings.json 	modified:   etrack/admin.py 	modified:   etrack/static/css/style.css 	new file:   etrack/static/images/idcard.jpg 	modified:   etrack/templates/etrack/elements/default.html 	modified:   etrack/templates/etrack/elements/index_content.html 	modified:   project_templates/admin/change_list.html 	new file:   project_templates/admin/change_list_object_tools.html

--- etrack/admin.py
+++ etrack/admin.py
@@ -4,40 +4,37 @@ from .models import Express
 import datetime
 from rangefilter.filter import DateRangeFilter
 from django.http import HttpResponse
-from import_export.admin import ExportActionMixin
+from import_export.admin import ImportMixin
 from import_export import resources
 
-# Register your models here.
 class MyAdminSite(admin.AdminSite):
     pass
-
-def upload_track_number(modeladmin, request, queryset):
-    pass
-upload_track_number.short_description = '上传快递单号的Excel文件'
-
 
 class ExpressResource(resources.ModelResource):
     class Meta:
         model = Express
-        
+        exclude = ('id', 'recipient_photo')
+    def get_export_headers(self):
+        headers = []
+        for field in self.get_fields():
+            model_fields = self.Meta.model._meta.get_fields()
+            header = next((x.verbose_name for x in model_fields if x.name == field.column_name), field.column_name)
+            headers.append(header)
+        return headers
 
+class ImportResource(resources.ModelResource):
+    class Meta:
+        model = Express
 
-
-class ExpressAdmin( admin.ModelAdmin):
+class ExpressAdmin(admin.ModelAdmin):
+    resource_class = ImportResource
     def download_excel(self, request, queryset):
         express_resource = ExpressResource()
         dataset = express_resource.export()
-        response = HttpResponse(dataset.xls, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = 'attachment; filename=liebaosudi_{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d-%H-%M'))
+        response = HttpResponse(dataset.xlsx, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=liebaosudi_{}.xlsx'.format(datetime.datetime.now().strftime('%Y%m%d-%H-%M'))
         return response
-    download_excel.short_description = '下载已选中的excel文件'
-
-    # disable delete action
-    # def get_actions(self, request):
-    #     actions = super().get_actions(request)
-    #     if 'delete_selected' in actions:
-    #         del actions['delete_selected']
-    #     return actions
+    download_excel.short_description = '选中并下载excel文件'
 
     fieldsets = [
         ('代理人', {'fields':['agent_account',],}),
@@ -51,7 +48,14 @@ class ExpressAdmin( admin.ModelAdmin):
     search_fields = ['sender_wechat_num','sender_name','sender_wechat_name','sender_mobile_num','shop','recipient_name', 'recipient_phone_num', 'recipient_country', 'recipient_province', 'recipient_city', 'recipient_district', 'recipient_addr', 'recipient_id', 'track_number','packet_state']
     list_per_page = 20
     actions = [download_excel,]
-    
+    def has_add_permission(self, request):
+        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+    def get_action_choices(self, request):
+        choices = super(ExpressAdmin, self).get_action_choices(request)
+        choices.pop(0)
+        return choices
 
 admin_site = MyAdminSite(name='myadmin')
 admin_site.register(Express, ExpressAdmin)
