e936a16b3695872237732d45b8d64e517a15aae4
mounan <1173359575zmn@gmail.com>
2021-04-25 21:06:24 +0900

    20210425_Z import excel add new actions  Changes to be committed: 	modified:   etrack/admin.py

--- etrack/admin.py
+++ etrack/admin.py
@@ -45,26 +45,41 @@ instance_loaders.ModelInstanceLoader.get_instance = get_instance
 class ImportResource(resources.ModelResource):
     track_number = fields.Field(column_name="申通单号", attribute="track_number")
     recipient_id = fields.Field(column_name="收件人身份证", attribute="recipient_id")
+    recipient_phone_num = fields.Field(column_name="收件人手机", attribute="recipient_phone_num")
+    recipient_name = fields.Field(column_name="收件人姓名", attribute="recipient_name")
     class Meta:
         model = Express
         report_skipped= True
         skip_unchanged = True
-        fields = ('track_number')
+        fields = ('track_number','recipient_id','recipient_phone_num', 'recipient_name')
+        # import_id_fields = ('recipient_id','track_number')
         import_id_fields = ('recipient_id',)
         exclude = ('id')
- 
+
+    def get_or_init_instance(self, instance_loader, row):
+        """
+        Either fetches an already existing instance or initializes a new one.
+        """
+        if not self._meta.force_init_instance:
+            instance = self.get_instance(instance_loader, row)
+            loacl_track_num = self.get_import_fields()[0].get_value(instance)
+            if loacl_track_num and loacl_track_num != row['申通单号']:
+                return (self.init_instance(row), True)
+            if instance:
+                return (instance, False)
+        return (self.init_instance(row), True)
+        
     def skip_row(self, instance, original):
         if self.get_import_fields()[0].get_value(original) != None:
             return True
-        if str(original) == '-----None-----None':
-            return True
+        # if str(original) == '-----None-----None':
+        #     return True
         return False
 
 class ExpressAdmin(ImportMixin, admin.ModelAdmin):
     # import excel
     
     resource_class = ImportResource
-
 
     def download_excel(self, request, queryset):
         express_resource = ExpressResource()
@@ -81,7 +96,7 @@ class ExpressAdmin(ImportMixin, admin.ModelAdmin):
         ('快递信息', {'fields':['track_number', 'packet_state']}),
     ]
     readonly_fields = ('agent_account','sender_wechat_num','sender_name','sender_wechat_name','sender_mobile_num','shop','sender_country','sender_province','sender_city','sender_district','sender_address','auto_recipient_name', 'recipient_name', 'recipient_phone_num', 'recipient_country', 'recipient_province', 'recipient_city', 'recipient_district', 'recipient_addr', 'recipient_id', 'recipient_photo','packet_state')
-    list_display = ('sender_wechat_num',  'track_number', 'created_date', 'packet_state')
+    list_display = ('packet_state', 'sender_wechat_num',  'track_number', 'created_date')
     list_filter = (('created_date',DateRangeFilter),'packet_state',)
     search_fields = ['sender_wechat_num','sender_name','sender_wechat_name','sender_mobile_num','shop','recipient_name', 'recipient_phone_num', 'recipient_country', 'recipient_province', 'recipient_city', 'recipient_district', 'recipient_addr', 'recipient_id', 'track_number','packet_state']
     list_per_page = 20
@@ -90,6 +105,7 @@ class ExpressAdmin(ImportMixin, admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
     def has_delete_permission(self, request, obj=None):
+        return True
         return False
     
     # remove '------' from actions select tag
